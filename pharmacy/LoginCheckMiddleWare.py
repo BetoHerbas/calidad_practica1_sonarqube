7c17140b73391196fd1191ae5db599d97c9fd7bb
BetoHerbas <betitoherbas@gmail.com>
2025-03-19 01:07:40 -0400

    Refactor LoginCheckMiddleware to reduce cognitive complexity by consolidating user type checks into a dictionary, simplifying the logic and improving maintainability

--- pharmacy/LoginCheckMiddleWare.py
+++ pharmacy/LoginCheckMiddleWare.py
@@ -12,63 +12,41 @@ from django.conf import settings
 #Check whether the user is logged in or not
 
 class LoginCheckMiddleWare(MiddlewareMixin):
-   
-    
+    # Define a mapping for user types and their corresponding view modules and redirection views
+    USER_TYPE_VIEWS = {
+        "1": ["pharmacy.HODViews"],
+        "2": ["pharmacy.pharmacistViews", "pharmacist_home"],
+        "3": ["pharmacy.DoctorViews", "doctor_home"],
+        "4": ["pharmacy.clerkViews", "clerk_home"],
+        "5": ["pharmacy.patient_view", "patient_home"],
+    }
+
     def process_view(self, request, view_func, view_args, view_kwargs):
         modulename = view_func.__module__
         user = request.user
 
+        # Check if the user is authenticated
         if user.is_authenticated:
-            if user.user_type == "1":
-                
-                if modulename == "pharmacy.HODViews": 
-                    pass
-                elif modulename == "":
-                    pass
-                elif modulename == "pharmacy.views" or modulename == "django.views.static":
-                    pass
-                else:
-                    pass
-            
-            elif user.user_type == "2":
-                if modulename == "pharmacy.pharmacistViews":
-                    pass
-                elif modulename == "pharmacy.views" or modulename == "django.views.static":
-                    pass
-                else:
-                    return redirect("pharmacist_home")
-            elif user.user_type == "3":
-                if modulename == "pharmacy.DoctorViews":
-                    pass
-                elif modulename == "pharmacy.views" or modulename == "django.views.static":
-                    pass
-                else:
-                    return redirect("doctor_home")
-            elif user.user_type == "4":
-                if modulename == "pharmacy.clerkViews":
-                    pass
-                elif modulename == "pharmacy.views" or modulename == "django.views.static":
-                    pass
-                else:
+            user_type = user.user_type
+            # Default allowed modules
+            allowed_modules = ["pharmacy.views", "django.views.static"]
 
-                    return redirect("clerk_home")
-            elif user.user_type == "5":
-                if modulename == "pharmacy.patient_view":
-                    pass
-                elif modulename == "pharmacy.views" or modulename == "django.views.static":
-                    pass
-                else:
-                    return redirect("patient_home")
-            
+            if user_type in self.USER_TYPE_VIEWS:
+                # Add allowed modules for user type
+                allowed_modules.append(self.USER_TYPE_VIEWS[user_type][0])
+                redirect_view = self.USER_TYPE_VIEWS[user_type][1] if len(self.USER_TYPE_VIEWS[user_type]) > 1 else None
+
+                # Check if the current module is allowed
+                if modulename not in allowed_modules and redirect_view:
+                    return redirect(redirect_view)
             else:
                 return redirect("login")
+        elif request.path != reverse("login"):
+            # If the user is not authenticated, redirect to login unless already on the login page
+            return redirect("login")
 
-            
-        else:
-            if request.path == reverse("login"):
-                pass
-            else:
-                return redirect("login")
+        return None
+
 
 
      #NB: Email confirmation will not occur       
